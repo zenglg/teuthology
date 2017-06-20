576006f34101e2f1310958b46fa1f00a7818276b
Vasu Kulkarni <vasu@redhat.com>
2017-06-20 14:35:09 -0700

    use site.yml.sample as default playbook
    we have customized playbook and using it in task, this can
    cause issue as we will miss out changes in the master repo and
    causing hard to debug issues.
    
    Signed-off-by: Vasu Kulkarni <vasu@redhat.com>

--- teuthology/task/ceph_ansible.py
+++ teuthology/task/ceph_ansible.py
@@ -19,49 +19,6 @@ log = logging.getLogger(__name__)
 class CephAnsible(Task):
     name = 'ceph_ansible'
 
-    _default_playbook = [
-        dict(
-            hosts='all',
-            become=True,
-            tasks=[],
-        ),
-        dict(
-            hosts='mons',
-            become=True,
-            roles=['ceph-mon'],
-        ),
-        dict(
-            hosts='mgrs',
-            become=True,
-            roles=['ceph-mgr'],
-        ),
-        dict(
-            hosts='osds',
-            become=True,
-            roles=['ceph-osd'],
-        ),
-        dict(
-            hosts='mdss',
-            become=True,
-            roles=['ceph-mds'],
-        ),
-        dict(
-            hosts='rgws',
-            become=True,
-            roles=['ceph-rgw'],
-        ),
-        dict(
-            hosts='clients',
-            become=True,
-            roles=['ceph-client'],
-        ),
-        dict(
-            hosts='restapis',
-            become=True,
-            roles=['ceph-restapi'],
-        ),
-    ]
-
     __doc__ = """
     A task to setup ceph cluster using ceph-ansible
 
@@ -85,16 +42,11 @@ class CephAnsible(Task):
         * Set ``public_network`` for each host if ``public_network`` is unset
     """.format(
         git_base=teuth_config.ceph_git_base_url,
-        playbook=_default_playbook,
     )
 
     def __init__(self, ctx, config):
         super(CephAnsible, self).__init__(ctx, config)
         config = self.config or dict()
-        if 'playbook' not in config:
-            self.playbook = self._default_playbook
-        else:
-            self.playbook = self.config['playbook']
         if 'repo' not in config:
             self.config['repo'] = os.path.join(teuth_config.ceph_git_base_url,
                                                'ceph-ansible.git')
@@ -114,19 +66,6 @@ class CephAnsible(Task):
         super(CephAnsible, self).setup()
         # generate hosts file based on test config
         self.generate_hosts_file()
-        # use default or user provided playbook file
-        pb_buffer = StringIO()
-        pb_buffer.write('---\n')
-        yaml.safe_dump(self.playbook, pb_buffer)
-        pb_buffer.seek(0)
-        playbook_file = NamedTemporaryFile(
-            prefix="ceph_ansible_playbook_",
-            dir='/tmp/',
-            delete=False,
-        )
-        playbook_file.write(pb_buffer.read())
-        playbook_file.flush()
-        self.playbook_file = playbook_file.name
         # everything from vars in config go into group_vars/all file
         extra_vars = dict()
         extra_vars.update(self.config.get('vars', dict()))
@@ -263,6 +202,10 @@ class CephAnsible(Task):
             '/usr/share/ceph-ansible',
             '.'
         ])
+        # use the site.yml.sample as playbook
+        ceph_installer.run(args=['cp',
+                                 'ceph-ansible/site.yml.sample',
+                                 'ceph-ansible/site.yml'])
         ceph_installer.put_file(self.inventory, 'ceph-ansible/inven.yml')
         ceph_installer.put_file(self.playbook_file, 'ceph-ansible/site.yml')
         # copy extra vars to groups/all
@@ -338,10 +281,12 @@ class CephAnsible(Task):
             run.Raw('-b %s' % branch),
             run.Raw(ansible_repo),
         ])
+        # use the site.yml.sample as playbook
+        ceph_installer.run(args=['cp',
+                                 'ceph-ansible/site.yml.sample',
+                                 'ceph-ansible/site.yml'])
         # copy the inventory file to installer node
         ceph_installer.put_file(self.inventory, 'ceph-ansible/inven.yml')
-        # copy the site file
-        ceph_installer.put_file(self.playbook_file, 'ceph-ansible/site.yml')
         # copy extra vars to groups/all
         ceph_installer.put_file(self.extra_vars_file, 'ceph-ansible/group_vars/all')
         # print for debug info
